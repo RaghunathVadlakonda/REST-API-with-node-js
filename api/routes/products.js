a1ed58031313153ed873940cc72bb1ff33570c36
RaghunathVadlakonda <vadlakonda.raghu5@gmail.com>
2018-10-13 16:42:37 +0530

    updated

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+// this route for shows all products
 router.get('/', (req, res, next) => {
     res.status(200).json({
         message : "GET request to /products"
     });
 });
 
+//this post route for adding product
 router.post('/',(req, res, next) => {
     res.status(200).json({
         message : "POST request to /products"
     });
 });
 
+// this productId route for particuler shows one product based on productId
 router.get('/:productId',(req, res, next) => {
     const id = req.params.productId;
     if(id === 'special'){
@@ -25,6 +28,20 @@ router.get('/:productId',(req, res, next) => {
                     message : "Hey this is productid!"
         });
     }
+});
+
+// this route for updating one product
+router.patch('/:productId',(req, res, next) => {
+    res.status(200).json({
+        message : " updated product!"
+    });
+});    
+
+// deleting product 
+router.delete('/:productId',(req, res, next) => {
+    res.status(200).json({
+        message : " delete product!"
+    });
 });
     
     
