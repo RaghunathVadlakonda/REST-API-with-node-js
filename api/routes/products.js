3cb241b4a47ac402825cd8f416c96ba1b1eeb153
RaghunathVadlakonda <vadlakonda.raghu5@gmail.com>
2018-10-13 15:58:44 +0530

    product post route and productId route

--- api/routes/products.js
+++ api/routes/products.js
@@ -7,5 +7,29 @@ router.get('/', (req, res, next) => {
     });
 });
 
+router.post('/',(req, res, next) => {
+    res.status(200).json({
+        message : "POST request to /products"
+    });
+});
+
+router.get('/:productId',(req, res, next) => {
+    const id = req.params.productId;
+    if(id === 'special'){
+        res.status(200).json({
+            message : "excutes when id is special",
+            id : id
+        });
+            } else {
+                     res.status(200).json({
+                    message : "Hey this is productid!"
+        });
+    }
+});
+    
+    
+
+
+
 
 module.exports = router;
