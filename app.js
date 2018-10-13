dab8db9529b37945a50b24d8fd92b038ecb75549
RaghunathVadlakonda <vadlakonda.raghu5@gmail.com>
2018-10-13 18:56:15 +0530

    updated routes and error handling

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+//error handling 
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+//error handling in application
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error : {
+        message : error.message
+         }
+    });
+});
+
 
 module.exports = app;
