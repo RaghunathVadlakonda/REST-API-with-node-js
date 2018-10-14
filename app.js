fd52d4343ccdb9c2ca6f867bc8975187f570306e
RaghunathVadlakonda <vadlakonda.raghu5@gmail.com>
2018-10-14 15:26:51 +0530

    updated CORS

--- app.js
+++ app.js
@@ -10,6 +10,20 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
+//CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Header',
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+if(req.method === 'OPTIONS'){
+    res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+    return res.status(200).json({});
+}
+    next();
+});
+
+//routes
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
